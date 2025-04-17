f78dff8e11740f4aedcb978c2e28c5e64c2f0aff
Lolo <loicfinette@gmail.com>
2025-04-17 16:27:03 +0200

    new message

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ def main():
     st.title("Hello, Streamlit!")
     st.write("This is a simple Streamlit app.")
     st.write("This is the modified title")
+    st.write("WOOHOOO")
 
 if __name__ == "__main__":
     main()
